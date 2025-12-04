b9edc49b7429291a35a5bd91d8286f5b1dce049f
Suleman <sulemanafandi18@gmail.com>
2025-12-04 10:35:36 +0500

    assigment01

---
+++ scritpt.js
@@ -0,0 +1,137 @@
+// ! -----Assigment 01 ----- 
+
+// ? chapter 01
+// Question 01
+alert('greet');
+
+// Question 2
+alert('Please! Enter a valid Number');
+
+// Question 4
+alert('Wellcome to Js Land\n Happy coding');
+
+//Question 5
+alert('Wellcome to JS land...')
+
+// Question 6
+alert('Happy Coding !')
+
+// Qusetion 7
+let a  = 'hello i run js code run in console';
+alert(a);
+
+console.log(a);
+
+
+
+? chapter 02
+Qusetion 1
+let username;
+console.log(username);
+
+// Question 2
+let fname = 'Muhammad';
+let lname = 'Suleman';
+console.log(fname , lname);
+
+// Question 3
+let title;
+title = 'Hello world';
+console.log(title);
+alert(title);
+
+// Question 4
+let myBio = 'My Name is M.Suleman';
+alert(myBio);
+console.log(myBio);
+
+
+// Question 5
+let name= `Pizza
+           Pizz
+           Piz
+           Pi
+           P
+`;
+alert(name);
+console.log(name);
+
+// Question 6
+let detail = 'My Email Address is ';
+let Email = `${detail} sulemanafandi18@gmail.com`;
+alert(Email)
+console.log(Email);
+
+// Question 7
+let choice = 'English';
+let book = `${choice} “A smarter way to learn JavaScript”.`
+alert(book);
+
+
+// Question 8
+let display = `“A smarter way to learn JavaScript”.`
+alert(display);
+document.write(display)
+console.log(display)
+
+// Question 9
+let show = "*****(●'◡'●)****";
+console.log(show);
+document.write(show);
+alert(show);
+
+
+
+// ? chapter 03
+
+// Question01
+let age = 19;
+alert(age);
+
+// Question 02
+var visits = 1;
+
+alert("You have visited this site " + visits + " time.");
+
+// Question 03
+let birth = '09/10/2004';
+document.write(birth);
+document.write(' Datatype of ',typeof birth);
+
+// Question 04
+let myname = 'Muhammad Suleman';
+let year = 2023;
+let certi = 'Certified Mobile Application Development';
+document.write('<br>','<br>', 'My name is ' + myname + '<br>' + 'I am ' + age + ' years old' + '<br>' + 'I was born in ' + birth + '<br>' + 'I am pursuing ' + certi + ' in the year ' + year);
+
+
+// ? chapter 04
+
+// Question 01
+// Declare 3 variables in one statement.
+var a = 1, b = 2, c = 3;
+console.log(a,b,c);
+
+// Question 02
+// Declare 5 legal & 5 illegal variable names.
+// Legal variable names
+var myName;
+var _age;
+var $salary;
+var first_name;
+var lastName123;
+console.log(myName, _age, $salary, first_name, lastName123);
+
+// Illegal variable names
+var 1stName;
+var my-name;
+var @age;
+var first name;
+var lastName!;
+
+// Question 03
+document.write('<h1>Rules for naming JS variables</h1>');
+document.write('Variable names can only contain letters, numbers, $ and _. For example $my_1stVariable<br>');
+document.write('Variables must begin with a letter, $ or _. For example $name, _name or name<br>');
+document.write('Variable names are case sensitive<br>');
+document.write('Variable names should not be JS keywords<br>');
